b73cba43f32707b301c2065ede32139c43b9fd6c
dmrolfs <WV#^MguUW&ar4tc8@UUv&*MxY%bFWf7nvfh>
2021-11-19 11:49:06 -0800

    add convenience ip_address parsing

--- src/common/http.rs
+++ src/common/http.rs
@@ -1,3 +1,5 @@
+use std::net::{AddrParseError, IpAddr};
+
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
 
@@ -7,4 +9,10 @@ pub struct HttpServerSettings {
     pub host: String,
     #[serde_as(as = "DisplayFromStr")]
     pub port: u16,
+}
+
+impl HttpServerSettings {
+    pub fn ip_address(&self) -> Result<IpAddr, AddrParseError> {
+        IpAddr::from_str(self.host)
+    }
 }
