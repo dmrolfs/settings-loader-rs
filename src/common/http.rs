e1957c0e15d5a2c8e6042bd201f25e13bb96afa2
dmrolfs <WV#^MguUW&ar4tc8@UUv&*MxY%bFWf7nvfh>
2021-11-22 13:09:59 -0800

    refactored http settings to support url

--- src/common/http.rs
+++ src/common/http.rs
@@ -1,8 +1,8 @@
-use std::net::{AddrParseError, IpAddr};
-use std::str::FromStr;
+use std::net::{AddrParseError, IpAddr, Ipv4Addr};
 
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
+use url::{Host, Origin, Url};
 
 #[serde_as]
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -13,7 +13,56 @@ pub struct HttpServerSettings {
 }
 
 impl HttpServerSettings {
-    pub fn ip_address(&self) -> Result<IpAddr, AddrParseError> {
-        IpAddr::from_str(self.host)
+    pub fn url_host(&self) -> Result<Host, url::ParseError> {
+        Host::parse(self.host.as_str())
+    }
+
+    pub fn url(&self, scheme: impl Into<String>) -> Result<Url, url::ParseError> {
+        let url_rep = format!("{}://{}:{}", scheme.into(), self.host, self.port);
+        Url::parse(url_rep.as_str())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claim::*;
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_url_host() {
+        let local = HttpServerSettings { host: "127.0.0.1".to_string(), port: 80 };
+        let actual = assert_ok!(local.url_host());
+        let expected = assert_ok!(Host::parse("127.0.0.1"));
+        assert_eq!(actual, expected);
+
+        let example = HttpServerSettings { host: "example.com".to_string(), port: 8080 };
+        let actual = assert_ok!(example.url_host());
+        let expected = assert_ok!(Host::parse("example.com"));
+        assert_eq!(actual, expected);
+
+        let dns = HttpServerSettings { host: "job_manager".to_string(), port: 8080 };
+        let actual = assert_ok!(dns.url_host());
+        let expected = assert_ok!(Host::parse("job_manager"));
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_url() {
+        let local = HttpServerSettings { host: "127.0.0.1".to_string(), port: 80 };
+        let actual = assert_ok!(local.url("https"));
+        let expected = assert_ok!(Url::parse("https://127.0.0.1:80"));
+        assert_eq!(actual, expected);
+
+        let example = HttpServerSettings { host: "example.com".to_string(), port: 8080 };
+        let actual = assert_ok!(example.url("http"));
+        let expected = assert_ok!(Url::parse("http://example.com:8080"));
+        assert_eq!(actual, expected);
+
+        let dns = HttpServerSettings { host: "job_manager".to_string(), port: 8888 };
+        let actual = assert_ok!(dns.url("https"));
+        let expected = assert_ok!(Url::parse("https://job_manager:8888"));
+        assert_eq!(actual, expected);
     }
 }
